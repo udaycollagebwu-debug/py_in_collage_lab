66af5094ba9016cc167453ce6ea84219140e62b7
Uday sankar <udaycollagebwu@gmail.com>
2026-07-09 16:28:43 +0530

    all day 1 code is don

---
+++ scond.py
@@ -0,0 +1,13 @@
+#(b) Wright a python program to purposfully riase indentatin error and correct it .
+
+# a=input("Enter a number :")
+# b=input("Enter a nomber :")
+#   c=a+b                     the error is hear 
+# print("The addtion is :",c)
+
+#the fixed code is hear 
+
+a=int(input("Enter a number :"))
+b=int(input("Enter a another number :"))
+c=a+b
+print("Addtion is : ",c)
