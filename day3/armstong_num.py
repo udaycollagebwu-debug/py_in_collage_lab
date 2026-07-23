5adee352193771fdb9f4c33c6095715060d32c72
Uday sankar <udaycollagebwu@gmail.com>
2026-07-23 15:39:45 +0530

    basic

--- day3/armstong_num.py
+++ day3/armstong_num.py
@@ -1,8 +1,7 @@
 #1. write a python code to check if a given number is Armstong No or not .
 
 # what is armostrong number -- example 153 = 1^3 + 5^3 + 3^3 = 153
-
-num=int(input("Enter a number: "))
+num=int(input("Enter a number for Armstrong check: "))
 sum=0
 temp=num
 while temp>0:
